85cfb326cbd3540c4ca6a24ff82b1db7241e9cbd
Robber <robber@Robbers-Mac.local>
2016-06-02 01:56:45 +0800

    Update seed.rb but still has bugs must fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
-post1 = Post.create(title: '妖精的尾巴/魔導少年 Zero', content: '妖精的尾巴ZERO動畫全集改編自真島浩創作的少年熱血類漫畫。')
+cate1 = Category.create(name: '最新話題')
+cate2 = Category.create(name: '年度排行')
+post1 = Post.create(title: '妖精的尾巴', content: '妖精的尾巴ZERO動畫全集改編自真島浩創作的少年熱血類漫畫。')
 post2 = Post.create(title: '暗殺教室', content: '《暗殺教室》描述揚言要炸掉地球的超生物「殺老師」，各同學想盡辦法殺死這名老師，故事圍繞著非日常的校園生活而展開。')
 
 ['這真是太捧了','給你一個讚！！'].each do |rc|
@@ -8,3 +10,7 @@ end
 ['太酷了','這部什麼時候上映呢？'].each do |rc|
   post2.comments.create(content: rc)
 end
+
+post1.categories << cate1
+post2.categories << cate1
+post1.categories << cate2
