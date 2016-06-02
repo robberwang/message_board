ea3369d721ed6a7a1d59b12e59b6156fbd8c415f
robber.wang <wang.robber@gmail.com>
2016-06-02 18:53:42 +0800

    Bugs had fixed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -70,6 +70,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :content, :image, category_ids: [] , comments_attributes: [ :id, :content, :_destroy])
+      params.require(:post).permit(:title, :content, :image, comments_attributes: [ :id, :content, :_destroy], category_ids: [])
     end
 end
